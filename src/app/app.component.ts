ad55ec4fda8aaf831aaadda5d2f3d45ac211d1c3
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-05-17 15:19:32 -0300

    Importando os componentes publicados com sucesso 🎉

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ButtonComponent } from '@alfabit/button';
+import { ButtonComponent } from '@org-teste/button';
+import { InputComponent } from '@org-teste/input';
+import { TesteComponent } from '@org-teste/teste';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonComponent],
+  imports: [CommonModule, RouterOutlet, ButtonComponent, InputComponent, TesteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
